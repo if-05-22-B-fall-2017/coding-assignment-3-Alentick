3714ad274f8d6361ad40ea7d49c024c612a010c0
Alen <Alentick@gmail.com>
2017-11-29 21:08:07 +0100

    Edit

---
+++ genealogy.pl
@@ -0,0 +1,30 @@
+parent(adam,john).
+parent(eve,john).
+parent(eve,lisa).
+parent(john,anne).
+parent(john,pat).
+parent(pat,jacob)
+parent(carol,jacob).
+
+/*parent(X,pat).*/
+
+/*parent(lisa,X).*/
+
+male(adam).
+male(john).
+female(eva).
+female(lisa).
+female(anne).
+male(pat).
+male(jacob).
+female(carol).
+
+grandparent(X,Y) :- parent(X,Z), parent(Z,Y).
+ancestor(X,Z) :- parent(X,Z).
+ancestor(X,Z) :- parent(X,Y), ancestor(Y,Z).
+
+/*[a,b,c].*/
+isHead([Head|_], Head).
+isTail([_|Tail], Tail).
+isMember(Head,[Head|_]).
+isMember(X,[_|Tail]) :- isMember(X,Tail).
